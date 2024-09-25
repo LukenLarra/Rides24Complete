9d4510c8abdea1cd2d5e005085eff1d2ee366c4c
LukenLarra <llarranaga024@ikasle.ehu.eus>
2024-09-25 17:48:18 +0200

    created class CreateRideMockTest

---
+++ src/test/java/testOperations/CreateRideMockTest.java
@@ -0,0 +1,49 @@
+package testOperations;
+
+import static org.junit.Assert.*;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javax.persistence.*;
+import org.junit.*;
+import org.mockito.*;
+
+import dataAccess.DataAccess;
+import domain.Driver;
+import exceptions.RideAlreadyExistException;
+import exceptions.RideMustBeLaterThanTodayException;
+
+public class CreateRideMockTest {
+
+	static DataAccess sut;
+	protected MockedStatic<Persistence> persistenceMock;
+	
+	@Mock
+	protected EntityManagerFactory entityManagerFactory;
+	
+	@Mock
+	protected EntityManager db;
+	
+	@Mock
+	protected EntityTransaction et;
+	
+	@Before
+	public void init() {
+		MockitoAnnotations.openMocks(this);
+		persistenceMock = Mockito.mockStatic(Persistence.class);
+		persistenceMock.when(() ->
+		Persistence.createEntityManagerFactory(Mockito.any())).thenReturn(entityManagerFactory);
+		Mockito.doReturn(db).when(entityManagerFactory).createEntityManager();
+		Mockito.doReturn(et).when(db).getTransaction();
+		sut=new DataAccess(db);
+	}	
+	
+	@After
+	public void tearDown() {
+		persistenceMock.close();
+	 }
+	
+	
+}
